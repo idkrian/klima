f3d7c7b023276027e8a8c55a72b0656073f1ae03
Rian Souza <riansouza0@gmail.com>
2021-09-11 02:02:30 -0300

    atualização css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
             <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <img src = {Logo} alt="Logo" className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" />
-                <span className="font-semibold text-xl tracking-tight">Clima</span>
+                <span className="font-semibold text-xl tracking-tight">Klima</span>
             </div>
             <div className="block lg:hidden">
                 <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
